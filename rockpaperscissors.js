d96f8a475728579d5db20fd5e7d2931743a3f18d
ClemoAcevedo <clementeacevedo@uc.cl>
2024-10-23 11:40:25 -0300

    feat(game logic): add getComputerChoice function to randomly return 'rock', 'paper', or 'scissors'

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,1 +1,15 @@
-console.log("Hello World")
+function getComputerChoice() {
+    let computerChoice;
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
+    if (randomNumber == 1) {
+        computerChoice = "rock";
+    }
+
+    else if (randomNumber == 2) {
+        computerChoice = "paper";
+    }
+
+    else if (randomNumber == 3) {
+        computerChoice = "scissors";
+    }
+};
